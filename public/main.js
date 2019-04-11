9aed6e951457d6fe52be15c51ef72fc1bb2ade1e
Nithin Stephen <nithinstephen1996@gmail.com>
2019-04-11 10:27:45 +0530

    fixed heroku bugs

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-let socket = io.connect('http://localhost:3000/')
+let socket = io.connect('https://new-realchat.herokuapp.com/')
 
 let outputNode = document.getElementById('outputNode');
 let inputText = document.getElementById('inputText');
